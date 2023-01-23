9276dfa11ebb932cd58a4c7219e5c23657e19519
Dario Ureña <darioaxel@gmail.com>
2023-01-23 08:46:00 +0100

    alineados top los elementos del blog

--- pages/myBlog.vue
+++ pages/myBlog.vue
@@ -1,6 +1,6 @@
 <template>
     <section
-        class=" h-full w-full flex flex-col justify-center items-center text-themeText text-center lg:text-left md:pt-6 bg-themeBackground">
+        class=" h-full w-full flex justify-center items-start text-themeText lg:text-left md:pt-6 bg-themeBackground">
         <div class="w-full lg:w-9/12 flex justify-center align-top">
             <div class="invisible lg:visible absolute z-10 bottom-1 left-1 text-white md:w-1/12 ml-3 mb-2">
                 <img src="~/assets/img/Space-Invaders-corner-left.svg">
