082c8a1660838d5670f9735aa0bf2b5f9d972f4b
dario ureña <darioaxel@gmail.com>
2023-01-25 17:40:01 +0100

    colocado un poco los datos

--- components/blog/CardHorizontal.vue
+++ components/blog/CardHorizontal.vue
@@ -2,11 +2,9 @@
     <div class="w-full flex flex-col place-content-start ml-5 mr-5">
         <NuxtLink class=" text-themePrimary text-2xl" :to="props.path">{{ props.title }}</NuxtLink>
         <div class=" flex flex-wrap">
-          <div class="">written-date</div>
-          <div class="">
-            <div v-for="tag in props.tags">
-              <BlogTag :title="tag" tagType="orange" />
-            </div>
+          <div class="">written-date</div>        
+            <div class=" flex" v-for="tag in props.tags">
+              <BlogTag class="ml-1 mr-1" :title="tag" tagType="orange" />        
             </div> 
         </div>
         {{ props.description }}  
