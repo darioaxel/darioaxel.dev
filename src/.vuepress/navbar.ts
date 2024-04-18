0c1a0060e34d5365ea09929c4312a46807b68b19
Dario Ureña <darioaxel@gmail.com>
2024-04-18 14:22:22 +0200

    trying to upgrade this

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -21,10 +21,5 @@ export default navbar([
         children: ["ray", { text: "...", icon: "ellipsis", link: "#" }],
       },
     ],
-  },
-  {
-    text: "V2 Docs",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/",
-  },
+  }
 ]);
